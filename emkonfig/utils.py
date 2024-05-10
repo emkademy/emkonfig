0e7a00d6b1795e653c504a8065f0f36064edc543
Kivanc Yuksel <kivanc.yuksel@allegro.com>
2024-05-10 16:23:13 +0200

    added exclude parameter to module importer

--- emkonfig/utils.py
+++ emkonfig/utils.py
@@ -2,7 +2,7 @@ import importlib
 import time
 
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 import yaml
 
@@ -17,7 +17,11 @@ def load_yaml(path: str) -> dict[str, Any]:
     return content
 
 
-def import_modules(dir_name: str, verbose: bool = False) -> None:
+def import_modules(dir_name: str, exclude: list[str] | set[str] | None = None, verbose: bool = False) -> None:
+    if exclude is None:
+        exclude = set()
+    exclude = set(exclude)
+
     start = time.time()
     for path in Path(dir_name).rglob("*.py"):
         if path.name.startswith("__"):
